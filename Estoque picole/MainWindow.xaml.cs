160cace0b857079ce799905619ebcf44e7060acd
gabriel <gabrielommello@gmail.com>
2025-11-13 20:45:35 -0300

    final, local

--- Estoque picole/MainWindow.xaml.cs
+++ Estoque picole/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace Estoque_picole
         {
 
             InitializeComponent();
-            ConexaoDp.AbrirConexao("server=sql10.freesqldatabase.com;user=sql10806094;password=knue8iZQp6;database=sql10806094;port=3306");
+            ConexaoDp.AbrirConexao("server=localhost;user=root;password=root;database=picole");
 
         }
     }
